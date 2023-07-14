98e9bd926bcc60f16972ebbc617dc2b08fcc987f
alicefirminger <alicefirminger@gmail.com>
2023-07-14 17:23:41 +0100

    functionality working how I want it. Need to ensure it is responsive

--- magiceightbored/src/Components/App/App.js
+++ magiceightbored/src/Components/App/App.js
@@ -2,18 +2,21 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 // import RNShake from 'react-native-shake';
 
-function App() {
+export default function App() {
 	const [randomActivity, setRandomActivity] = useState(null);
 	const [isBallClicked, setIsBallClicked] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		async function fetchActivity() {
 			const response = await fetch(`https://www.boredapi.com/api/activity/`);
 			const data = await response.json();
 			setRandomActivity(data);
+			setIsLoading(false);
 			console.log(data);
 		}
 		if (isBallClicked) {
+			setIsLoading(true);
 			fetchActivity();
 		}
 	}, [isBallClicked]);
@@ -32,21 +35,35 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>Magic 8 Bored 🥱</h1>
-			<div
-				className={`magicBall ${isBallClicked ? "clicked" : ""}`}
-				onClick={handleClick}
-			>
-				<div className="activity">
-					{randomActivity && randomActivity.activity}
+			<div className="contentContainer">
+				<header className="header">
+					<h1>Magic 8 Bored 🥱</h1>
+				</header>
+				<h3 className="howTo">
+					Feeling bored? Click on the Magic 8 Ball and it will tell you what to
+					do
+				</h3>
+				<div
+					className={`magicBall ${isBallClicked ? "clicked" : ""}`}
+					onClick={handleClick}
+				>
+					{isLoading ? (
+						<div className="loading"></div>
+					) : (
+						isBallClicked &&
+						randomActivity && (
+							<div className="activity">{randomActivity.activity}</div>
+						)
+					)}
 				</div>
+				<button className="resetButton" onClick={handleReset}>
+					Reset
+				</button>
 			</div>
-			<button onClick={handleReset}>Reset</button>
-			<h3>
-				Feeling bored? Click on the Magic 8 Ball and it will tell you what to do
-			</h3>
+			<footer className="footer">
+				(c) 2023 Alice Firminger All Rights Reserved
+			</footer>
 		</div>
 	);
-}
+};
 
-export default App;
